4f8fa2c1bdac1895213e403725fa0a2ea50c3d94
Matisse <matissefamularo@gmail.com>
2020-10-08 14:45:07 +0200

    Minor changes, started carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,20 @@
+const box1 = document.querySelector('#article1');
+
+const box2 = document.querySelector('#article2');
+
+const box3 = document.querySelector('#article3');
+
+const box4 = document.querySelector('#article4');
+
+const goRight = document.querySelector('.buttonRight')
+
+box2.classList.add("backBox");
+
+box3.classList.add("backBox");
+
+box4.classList.add("backBox");
+
+const activeBox = 1
+
+// if (activeBox = 1 ) {
+
